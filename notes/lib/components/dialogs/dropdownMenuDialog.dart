d2d0a2ecbb3c0b5edd8e4b09447f909835f8a1c2
Lukas Runt <lukas.runt@seznam.cz>
2024-03-29 23:05:14 +0100

    Added test on dropdownDialog

--- notes/lib/components/dialogs/dropdownMenuDialog.dart
+++ notes/lib/components/dialogs/dropdownMenuDialog.dart
@@ -2,17 +2,84 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:notes/components/componentUtils.dart';
 
+/// A [DropdownMenuDialog] widget that presents a dropdown menu inside an alert dialog.
+///
+/// This widget is designed to offer a selection from a dropdown menu with the option to confirm or cancel the action.
+/// It is customizable through parameters for title, confirm button text, dropdown items, and callbacks for each action.
+///
+/// Parameters:
+/// - [onConfirm]: A callback function that is executed when the confirm button is pressed.
+/// - [onCancel]: A callback function that is executed when the cancel button is pressed.
+/// - [titleText]: Text to display as the title of the dialog.
+/// - [confirmButtonText]: Text to display on the confirm button.
+/// - [items]: A list of [DropdownMenuItem<String>] to display in the dropdown.
+/// - [selectedValue]: The currently selected value in the dropdown. Can be `null` initially.
+/// - [onSelect]: A callback that is executed when a new item is selected from the dropdown.
+///
+/// Usage example:
+/// ```dart
+/// DropdownMenuDialog(
+///   onConfirm: () {
+///     // Handle confirmation action
+///   },
+///   onCancel: () {
+///     // Handle cancellation action
+///   },
+///   titleText: 'Select Option',
+///   confirmButtonText: 'Confirm',
+///   items: [
+///     DropdownMenuItem<String>(
+///       value: 'Option 1',
+///       child: Text('Option 1'),
+///     ),
+///     DropdownMenuItem<String>(
+///       value: 'Option 2',
+///       child: Text('Option 2'),
+///     ),
+///   ],
+///   onSelect: (value) {
+///     // Handle selection change
+///   },
+/// )
+/// ```
+///
+/// This dialog leverages [AlertDialog] to present its content, providing a consistent and familiar UI for users.
+// ignore: must_be_immutable
 // ignore: must_be_immutable
 class DropdownMenuDialog extends StatelessWidget {
+  /// A callback function that is called when the user presses the confirm button.
+  ///
+  /// Use this to define the action that should be taken when the user confirms action.
   final VoidCallback onConfirm;
+
+  /// A callback function that is called when the user presses the cancel button.
+  ///
+  /// Use this to define the action that should be taken when the user wants to cancel the action.
   final VoidCallback onCancel;
+
+  /// The text to be displayed as the title of the dialog.
   final String titleText;
+
+  /// The text of what will be shown in the confirm button.
   final String confirmButtonText;
+
+  /// A list of [DropdownMenuItem<String>] to display in the dropdown.
   final List<DropdownMenuItem<String>> items;
+
+  /// The currently selected value in the dropdown. Can be `null` initially.
   String? selectedValue;
+
+  ///  A callback that is executed when a new item is selected from the dropdown.
   final Function(String?) onSelect;
+
+  /// An instance of [ComponentUtils] for utility functions and styling.
   final utils = ComponentUtils();
 
+  /// Constructor of the [DropdownMenuDialog]
+  ///
+  /// [onConfirm] and [onCancel] must not be null. They define the callback functions
+  /// for the confirm and cancel actions, respectively. [titleText] and [contentText]
+  /// provide the text displayed in the dialog.
   DropdownMenuDialog({
     super.key,
     required this.onConfirm,
@@ -24,6 +91,7 @@ class DropdownMenuDialog extends StatelessWidget {
     this.selectedValue,
   });
 
+  // Builds the UI of the textfield dialog.
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -48,6 +116,7 @@ class DropdownMenuDialog extends StatelessWidget {
       ),
       actions: [
         TextButton(
+          key: const Key('cancelButton'),
           onPressed: onCancel,
           style: utils.getButtonStyle(),
           child: Text(AppLocalizations.of(context)!.cancel),
---
+++ notes/test/widget_test/components/dropdownMenuDialog_test.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:notes/components/dialogs/dropdownMenuDialog.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  // Define test widgets and necessary mock callbacks
+  final List<DropdownMenuItem<String>> testItems = [
+    const DropdownMenuItem(value: "1", child: Text("Item 1")),
+    const DropdownMenuItem(value: "2", child: Text("Item 2")),
+  ];
+
+  String? selectedValue;
+
+  group('DropdownMenuDialog Tests', () {
+    testWidgets('Confirm button trigers onConfirm callback', (WidgetTester tester) async {
+      bool confirmPressed = false;
+      await tester.pumpWidget(
+        MaterialApp(
+          home: DropdownMenuDialog(
+            onConfirm: () => confirmPressed = true,
+            onCancel: () {},
+            titleText: 'Select Option',
+            confirmButtonText: 'Confirm',
+            items: testItems,
+            onSelect: (value) {
+              selectedValue = value;
+            },
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      await tester.tap(find.text('Confirm'));
+      await tester.pump();
+
+      expect(confirmPressed, isTrue);
+    });
+
+    testWidgets('Confirm button trigers onCancel callback', (WidgetTester tester) async {
+      bool cancelPressed = false;
+      await tester.pumpWidget(
+        MaterialApp(
+          home: DropdownMenuDialog(
+            onConfirm: () {},
+            onCancel: () => cancelPressed = true,
+            titleText: 'Select Option',
+            confirmButtonText: 'Confirm',
+            items: testItems,
+            onSelect: (value) {
+              selectedValue = value;
+            },
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      await tester.tap(find.byKey(const Key('cancelButton')));
+      await tester.pump();
+
+      expect(cancelPressed, isTrue);
+    });
+
+    testWidgets('displays correctly with title and confirm button', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: DropdownMenuDialog(
+            onConfirm: () {},
+            onCancel: () {},
+            titleText: 'Select Option',
+            confirmButtonText: 'Confirm',
+            items: testItems,
+            onSelect: (value) {
+              selectedValue = value;
+            },
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      expect(find.text('Select Option'), findsOneWidget);
+      expect(find.text('Confirm'), findsOneWidget);
+    });
+
+    testWidgets('dropdown selection updates value', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: DropdownMenuDialog(
+            onConfirm: () {},
+            onCancel: () {},
+            titleText: 'Select Option',
+            confirmButtonText: 'Confirm',
+            items: testItems,
+            onSelect: (value) {
+              selectedValue = value;
+            },
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        ),
+      );
+
+      await tester.tap(find.byType(DropdownButtonFormField<String>));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Item 1').last);
+      await tester.pumpAndSettle();
+
+      expect(selectedValue, '1');
+    });
+  });
+}
