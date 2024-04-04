9860830716d68542aeaba8b79d4279df9e4a7412
Lukas Runt <lukas.runt@seznam.cz>
2024-04-04 08:07:51 +0200

    Adding utli for showing the time of last sync

--- notes/lib/services/utilService.dart
+++ notes/lib/services/utilService.dart
@@ -2,7 +2,6 @@ import 'package:intl/intl.dart';
 
 class UtilService {
   static String getFormattedDate(int millisecondsSinceEpoch) {
-    print(millisecondsSinceEpoch);
     DateTime date = DateTime.fromMicrosecondsSinceEpoch(millisecondsSinceEpoch);
     return DateFormat('HH:mm dd.MM.yyyy').format(date);
   }
