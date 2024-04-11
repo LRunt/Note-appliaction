23a466b505974b31013a270d5e98d570e1808893
Lukas Runt <lukas.runt@seznam.cz>
2024-04-11 20:05:51 +0200

    Writing UnitTest for user database

--- notes/test/unit_test/database/userDatabase_test.dart
+++ notes/test/unit_test/database/userDatabase_test.dart
@@ -1,13 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:notes/boxes.dart'; // Adjust the import based on your project structure
+import 'package:notes/boxes.dart';
 import 'package:notes/assets/constants.dart';
 import 'package:notes/data/userDatabase.dart';
 import 'package:hive/hive.dart';
-import 'package:flutter/widgets.dart';
 
-// Use the mock box instead of the real Hive box
-class MockBox extends Mock implements Box {}
+// Using the mock box instead of the real Hive box
+class MockBox extends Mock implements Box {
+  @override
+  Future<void> put(key, value) => super.noSuchMethod(
+        Invocation.method(#put, [key, value]),
+        returnValue: Future.value(),
+        returnValueForMissingStub: Future.value(),
+      );
+}
 
 void main() {
   group('UserDatabase Tests', () {
@@ -20,7 +27,7 @@ void main() {
       boxUser = mockBox;
     });
 
-    /*test('saveLocale saves the locale string', () {
+    test('saveLocale saves the locale string', () {
       const String langCode = 'en';
 
       // Act
@@ -28,9 +35,9 @@ void main() {
 
       // Assert
       verify(mockBox.put(LOCALE, langCode)).called(1);
-    });*/
+    });
 
-    test('loadLocale returns the correct Locale instance', () {
+    test('loadLocale returns correct Locale instance', () {
       // Arrange
       const String langCode = 'en';
       when(mockBox.get(LOCALE)).thenReturn(langCode);
@@ -41,6 +48,47 @@ void main() {
       // Assert
       expect(result, isA<Locale>());
       expect(result.languageCode, equals(langCode));
+    });
+
+    test('saveTheme stores theme string', () {
+      // Act
+      userDatabase.saveTheme(ThemeMode.light);
+
+      // Assert
+      verify(mockBox.put(THEME, LIGHT)).called(1);
+    });
+
+    test('loadTheme returns correct ThemeMode - light', () {
+      // Arrange
+      when(mockBox.get(THEME, defaultValue: SYSTEM)).thenReturn(LIGHT);
+
+      // Act
+      final result = userDatabase.loadTheme();
+
+      // Assert
+      expect(result, ThemeMode.light);
+    });
+
+    test('loadTheme returns correct ThemeMode - dark', () {
+      // Arrange
+      when(mockBox.get(THEME, defaultValue: SYSTEM)).thenReturn(DARK);
+
+      // Act
+      final result = userDatabase.loadTheme();
+
+      // Assert
+      expect(result, ThemeMode.dark);
+    });
+
+    test('loadTheme returns correct ThemeMode - system (default)', () {
+      // Arrange
+      when(mockBox.get(THEME, defaultValue: SYSTEM)).thenReturn(SYSTEM);
+
+      // Act
+      final result = userDatabase.loadTheme();
+
+      // Assert
+      expect(result, ThemeMode.system);
     });
   });
 }
